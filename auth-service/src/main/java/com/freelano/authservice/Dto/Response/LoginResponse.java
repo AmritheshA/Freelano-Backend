cf2e837195ad91b8dd84cb50278d9df53d62f063
AmritheshA <amrithesh0000@gmail.com>
2024-03-22 01:03:45 +0530

    fix : LoginResponse Updated

--- auth-service/src/main/java/com/freelano/authservice/Dto/Response/LoginResponse.java
+++ auth-service/src/main/java/com/freelano/authservice/Dto/Response/LoginResponse.java
@@ -1,4 +1,15 @@
 package com.freelano.authservice.Dto.Response;
 
+import lombok.*;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginResponse {
+
+    private String accessToken;
+    private String message;
+
 }
